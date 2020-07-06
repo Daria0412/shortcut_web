baf7a8951a256b6cf8406ec7dacd669dd24f6dc5
daria0412 <s2018w29@icloud.com>
2020-07-06 10:31:09 +0900

    show_list_1 url

--- web/urls.py
+++ web/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('list/', views.show_list, name = 'show_list'),
+    path('list/', views.show_list_1, name = 'show_list_1'),
     path('list/delete/', views.delete, name = 'delete'),
     path('list/<int:no>/<int:page>', views.show_list, name = 'show_list'),
     path('register/',views.register, name = 'register'),
